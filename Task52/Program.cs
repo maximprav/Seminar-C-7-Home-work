522de8a2217e52b532a1e470ab84e7bfff9cc1fb
Maxim Prav <mxmlknv@mail.ru>
2023-03-01 17:55:07 +0300

    Записано условие задачи 52. Решить!

---
+++ Task52/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Console.WriteLine("Введите количество строк массива:");
+int rows = int.Parse(Console.ReadLine()!)!;
+Console.WriteLine("Введите количество столбцов массива:");
+int columns = int.Parse(Console.ReadLine()!)!;
+
+int[,] array = GetArray(rows, columns, -10, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
